640ae058724a31a8031bdeda08618d9ac7c1b5b9
mg-recruitment-dev <113239194+mg-recruitment-dev@users.noreply.github.com>
2022-09-11 20:24:30 +0200

    Step 6 - frame rate display

---
+++ Assets/Code/InfoDisplay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class InfoDisplay : MonoBehaviour
+{
+    [SerializeField] private TMP_Text infoLabel;
+    [SerializeField] private float fpsUpdateFrequency = 1f;
+
+    private float timer;
+    private int frameCount;
+
+    private void Update()
+    {
+        timer += Time.unscaledDeltaTime;
+        frameCount++;
+
+        if ( timer < fpsUpdateFrequency )
+            return;
+
+        UpdateLabel();
+
+        timer = 0f;
+        frameCount = 0;
+    }
+
+    private void UpdateLabel()
+    {
+        float averageFramerate = frameCount / timer;
+        infoLabel.text = $"FPS: {averageFramerate.ToString( "0.0" )} (sampled over {fpsUpdateFrequency}s)";
+    }
+}
